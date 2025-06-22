c470c3cd2ba0b6bdd6ef8fb0bfa17f6dd8f43c78
MrBasuraHead <papastavropoulos.k@gmail.com>
2025-06-22 12:13:08 -0400

    B: Black Window and Close button

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting Asteroids!")
-    print ("Screen width: 1280") 
-    print("Screen height: 720")
-
+    pygame.init()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        screen.fill("black",rect=None, special_flags=0)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
