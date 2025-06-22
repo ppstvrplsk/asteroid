86bbe3f843c713b02fe1a68f07ebce1f4bc6a819
MrBasuraHead <papastavropoulos.k@gmail.com>
2025-06-22 11:55:30 -0400

    A: Start of Asteroid

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+from constants import *
+
+def main():
+    print("Starting Asteroids!")
+    print ("Screen width: 1280") 
+    print("Screen height: 720")
+
+
+if __name__ == "__main__":
+    main()
