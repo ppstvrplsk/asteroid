1e95b13c1432dc18eaddedc89ef2e8968ad3a2f1
MrBasuraHead <papastavropoulos.k@gmail.com>
2025-06-28 12:46:46 -0400

    M: Shots killing Asteroids

--- main.py
+++ main.py
@@ -40,6 +40,11 @@ def main():
             if asteroid.collision(player):
                 print("Game Over!")
                 sys.exit()
+        
+            for shot in shots:
+                if asteroid.collision(shot):
+                    asteroid.kill()
+                    shot.kill()
 
         screen.fill("black")
         
